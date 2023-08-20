ba7084b89557b44ac294c47e99d16baa941b3b3c
CLEETO <cleeto2003@gmail.com>
2023-08-20 15:00:02 +0530

    delete

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Listing,UserProfile,userwishlist,modeldata
-
-admin.site.register(UserProfile)
-admin.site.register(Listing)
-admin.site.register(userwishlist)
-admin.site.register(modeldata)
-
-# Register your models here.
